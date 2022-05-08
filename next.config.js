ac3828f78009cbd7d2a587e7f6b3b3c4148c5126
EnesKilicaslan <eeneskilicaslan@gmail.com>
2022-05-08 03:13:38 -0700

    fix deploy images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
+  distDir: "build",
+  // basePath: isProd ? "/twitter-clone" : "",
+  // assetPrefix: isProd ? "/twitter-clone" : "",
+  images: {
+    loader: "custom",
+    domains: ["pbs.twimg.com"],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
