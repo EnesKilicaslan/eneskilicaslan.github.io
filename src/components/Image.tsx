a1414ae4a31b673ec62fe522adf6f85ca0c9fe52
EnesKilicaslan <eeneskilicaslan@gmail.com>
2022-05-10 14:45:32 -0700

    Update image size

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -3,13 +3,12 @@ import { imageLoader } from "../../helpers/image_loader";
 
 export const Image = ({ src }: { src: string }) => {
   return (
-    // <div className="img-container">
     <div className="img-container">
       <Img
         loader={imageLoader}
         src={src}
         unoptimized={true}
-        objectFit="fill"
+        objectFit="none"
         layout="fill"
       />
     </div>
